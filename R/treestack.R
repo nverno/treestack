4629a238f4e03e5f85cb09da30316d644a1430a0
Noah Peart <noah.v.peart@gmail.com>
2015-11-07 23:29:58 -0500

    init

---
+++ R/treestack.R
@@ -0,0 +1,5 @@
+#' reestack: a package to make stacked bar version of treemaps
+#' @docType package
+#' @name treestack
+NULL
+#> NULL
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(treestack)
+
+## test_check("treestack")
